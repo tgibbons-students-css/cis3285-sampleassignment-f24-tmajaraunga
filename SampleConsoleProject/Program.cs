08cd3475fc64b183cc27ed8ff5a66bb6112eed2b
tmajaraunga <tmajaraunga@css.edu>
2024-09-17 20:22:36 -0500

    Taku's comment to cloned repository

--- SampleConsoleProject/Program.cs
+++ SampleConsoleProject/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Adding a line in Visual Studio");
 // added this line -- added to comment
 Console.WriteLine("Adding a line in VS Code");
-
+//Taku added comment 
